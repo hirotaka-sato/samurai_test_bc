a501d5ce181692f51e7d603fd05f630cbe969d01
hirotaka-sato <hirotaka.sato.jp@gmail.com>
2019-07-02 00:21:12 +0900

    Practice-4

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -1,38 +1,26 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
 class PracticeController extends Controller
 {
-
     public function index() {
-        $data = array();
         $data = [
-            60, 50, 90, 70, 80
+            'msg'=>'Input A (Odd or Even) = ?',
         ];
-        $data1 = $data;
-        $tmpmax = max($data);
-        $tmp = 0;
-        $data1 = [0=>$tmpmax];
-        for($j=1; $j < count($data); $j++) {
-            for($i=0; $i < count($data); $i++) {
-                if(($data[$i] < $tmpmax) && ($data[$i] >= $tmp)) {
-                    $tmp = $data[$i];
-                }
-            }
-            $data1[$j] = $tmp;
-            $tmpmax = $tmp;
-            $tmp = 0;
-        }
-        return view('practice.index',compact('data1'));
-        
+        return view('practice.index', $data);
     }
-    //public function post(Request $request) {
-        // $a = $request->a;
-        // $b = $request->b;
-        //$msg = print_r($data);
-        //return view('practice.index', $data);
-    //}
+    public function post(Request $request) {
+        $a = $request->a;
+        //$b = $request->b;
+        $b = (int)($a % 2);
+        if ($b == 0) {
+            $msg = 'Even Number';
+        } else {
+            $msg = 'Odd Number';
+        }
+        $data = [
+            'msg'=>'A is ' . $msg . ' !',
+        ];
+        return view('practice.index', $data);
+    }
 }
