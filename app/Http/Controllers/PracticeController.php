1a1b426c9110c65ee0748bca7355815a30d7c65f
hirotaka-sato <hirotaka.sato.jp@gmail.com>
2019-07-02 00:10:28 +0900

    Practice-2

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -1,38 +1,21 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
 class PracticeController extends Controller
 {
-
     public function index() {
-        $data = array();
         $data = [
-            60, 50, 90, 70, 80
+            'msg'=>'Sir Name + First Name = ?',
         ];
-        $data1 = $data;
-        $tmpmax = max($data);
-        $tmp = 0;
-        $data1 = [0=>$tmpmax];
-        for($j=1; $j < count($data); $j++) {
-            for($i=0; $i < count($data); $i++) {
-                if(($data[$i] < $tmpmax) && ($data[$i] >= $tmp)) {
-                    $tmp = $data[$i];
-                }
-            }
-            $data1[$j] = $tmp;
-            $tmpmax = $tmp;
-            $tmp = 0;
-        }
-        return view('practice.index',compact('data1'));
-        
+        return view('practice.index', $data);
     }
-    //public function post(Request $request) {
-        // $a = $request->a;
-        // $b = $request->b;
-        //$msg = print_r($data);
-        //return view('practice.index', $data);
-    //}
+    public function post(Request $request) {
+        $a = $request->a;
+        $b = $request->b;
+        $msg = $a . $b;
+        $data = [
+            'msg'=>'Your Name is ' . $msg . ' !',
+        ];
+        return view('practice.index', $data);
+    }
 }
