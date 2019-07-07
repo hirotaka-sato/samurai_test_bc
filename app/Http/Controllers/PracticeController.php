5b74ae348a72e219d24c0c279118cf9bdbf3c667
hirotaka-sato <hirotaka.sato.jp@gmail.com>
2019-07-07 23:56:44 +0900

    Laravel Practice ch3-26

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -5,19 +5,16 @@ class PracticeController extends Controller
 {
     public function index() {
         $data = [
-            'ccc', 'bbb', 'aaa'
+            'msg'=>'aaa'
         ];
-        $data1 = [
-            0=>'aaa', 2=>'ccc'
-        ];
-        $replace = array_replace($data, $data1);
-        return view('practice.index',compact('replace'));
+        return view('practice.index',compact('data'));
         
     }
-    //public function post(Request $request) {
-        // $a = $request->a;
-        // $b = $request->b;
-        //$msg = print_r($data);
-        //return view('practice.index', $data);
-    //}
+    public function post(Request $request) {
+        $msg = $request->msg;
+        $data = [
+            'msg'=>'bbb'
+        ];
+        return view('practice.index', $data);
+    }
 }
--- resources/views/practice/index.blade.php
+++ resources/views/practice/index.blade.php
@@ -1,27 +1,17 @@
-<html>
-<head>
-    <title>Hello/Index</title>
-	<style>
-	body {font-size:16pt; color:#999;}
-	h1 {font-size:50pt; text-align:right; color:#f6f6f6;
-	    margin:-20px 0px -30px 0px; letter-spacing:-4pt;}
-	</style>
-</head>
-<body>
-	<h1>演習問14</h1>
+@extends('layouts.practice')
 
-    <ol>
-    @foreach($replace as $msg)
-    <li>&nbsp;&nbsp;{{$msg}}
-    @endforeach
-    </ol>
+@section('title', 'Index')
 
+@section('menubar')
+    @parent
+    インデックスページ
+@endsection
 
-    <!-- <form method="POST" action="/practice">
-        {{ csrf_field() }}
-        <input type="text" name="a">
-        <input type="text" name="b">
-        <input type="submit">
-    </form> -->
-</body>
-</html>
+@section('content')
+    <p>ここが本文のコンテンツです。</p>
+    <p>必要なだけ記述できます。</p>
+@endsection
+
+@section('footer')
+copyright 2019 sato.
+@endsection
