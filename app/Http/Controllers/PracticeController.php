bacd3d2ddfb18444dceeed6119c770249f3db43c
hirotaka-sato <hirotaka.sato.jp@gmail.com>
2019-07-09 00:09:28 +0900

    Laravel Practice ch5-8

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -6,7 +6,12 @@ use Illuminate\Support\Facades\DB;
 class PracticeController extends Controller
 {
     public function index(Request $request) {
-        $items = DB::select('select * from people');
+        if (isset($request->id)) {
+            $param = ['id'=>$request->id];
+            $items = DB::select('select * from people where id = :id', $param);
+        } else {
+            $items = DB::select('select * from people');
+        }
         return view('practice.index', ['items'=>$items]);
     }
 }
