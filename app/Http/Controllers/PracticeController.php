fbe8d0b5e0aa54b4d5cc7f001cb26ac94648dd34
hirotaka-sato <hirotaka.sato.jp@gmail.com>
2019-07-11 23:31:03 +0900

    Laravel Practice ch4-24

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -8,7 +8,16 @@ class PracticeController extends Controller
 {
     public function index(Request $request) {
 
-        return view('practice.index', ['msg'=>'フォームを入力：']);
+        $validator = Validator::make($request->query(), [
+            'id' => 'required',
+            'pass' => 'required',
+        ]);
+        if ($validator->fails()) {
+            $msg = 'クエリーに問題があります。';
+        } else {
+            $msg = 'ID/PASSを受け付けました。フォームを入力してください。';
+        }
+        return view('practice.index', ['msg'=>$msg,]);
         
     }
 
