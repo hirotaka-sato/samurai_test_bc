627ee40c673a6100ce81b2dcbc107e4f7ef71ccb
hirotaka-sato <hirotaka.sato.jp@gmail.com>
2019-07-11 23:18:04 +0900

    Laravel Practice ch4-23

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\PracticeRequest;
+use Validator;
 //use App\Http\Middleware\PracticeMiddleware;
 class PracticeController extends Controller
 {
@@ -11,10 +12,19 @@ class PracticeController extends Controller
         
     }
 
-    public function post(PracticeRequest $request) {
+    public function post(Request $request) {
         
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'mail' => 'email',
+            'age' => 'numeric|between:0,150',
+        ]);
+        if ($validator->fails()) {
+            return redirect('/practice')
+                ->withErrors($validator)
+                ->withInput();
+        }
         return view('practice.index', ['msg'=>'正しく入力されました！']);
-
     }
 
 }
