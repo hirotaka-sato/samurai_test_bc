37d89b4d3b9befdb7cb55be099d29906b5b93c29
hirotaka-sato <hirotaka.sato.jp@gmail.com>
2019-07-14 20:52:03 +0900

    Laravel Practice ch5-26

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\DB;
 class PracticeController extends Controller
 {
     public function index(Request $request) {
+
         $items = DB::table('people')->get();
         return view('practice.index', ['items'=>$items]);
     }
@@ -21,21 +22,24 @@ class PracticeController extends Controller
     }
     
     public function post(Request $request) {
+
         $items = DB::select('select * from people');
         return view('practice.index', ['items'=>$items]);
     }
 
     public function add(Request $request) {
+
         return view('practice.add');
     }
 
     public function create(Request $request) {
+
         $param = [
             'name' => $request->name,
             'mail' => $request->mail,
             'age' => $request->age,
         ];
-        DB::insert('insert into people (name, mail, age) values(:name, :mail, :age)', $param);
+        DB::table('people')->insert($param);
         return redirect('/practice');
     }
 
