46d9aab71069a9bbeedb2d3b2f1390889a5a8bec
hirotaka-sato <hirotaka.sato.jp@gmail.com>
2019-07-14 18:05:11 +0900

    Laravel Practice ch5-24

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -11,10 +11,10 @@ class PracticeController extends Controller
     }
 
     public function show(Request $request) {
-        $name = $request->name;
+        $min = $request->min;
+        $max = $request->max;
         $items = DB::table('people')
-            ->where('name', 'like', '%' . $name . '%')
-            ->orWhere('mail', 'like', '%' . $name . '%')
+            ->whereRaw('age >= ? and age <= ?', [$min, $max])
             ->get();
         return view('practice.show', ['items'=>$items]);
     }
