8d433ac33c6c00af2b6bd610e011602e4d7f10e3
hirotaka-sato <hirotaka.sato.jp@gmail.com>
2019-07-17 00:18:28 +0900

    Laravel Practice ch6-11

--- app/Person.php
+++ app/Person.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Person extends Model
 {
     public function getData() {
+
         return $this->id . ': ' . $this->name .' (' . $this->age . ')';
+    }
+
+    public function scopeNameEqual($query, $str) {
+
+        return $query->where('name', $str);
     }
 }
